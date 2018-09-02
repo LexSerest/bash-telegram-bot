c6b686e222299abfcc82c1f91bb36b8817d458af
Lex Serest <lexserest@gmail.com>
2018-09-02 18:49:08 +0300

    tabs to space

--- main.sh
+++ main.sh
@@ -8,16 +8,16 @@ function escape(){
 }
 
 function reply_send(){
-	curl -s "$BOT_API_URL/sendMessage" \
-	--data-urlencode "chat_id=$1" \
-	--data-urlencode "reply_to_message_id=$2" \
-	--data-urlencode "text=$3" >> /dev/null
+    curl -s "$BOT_API_URL/sendMessage" \
+    --data-urlencode "chat_id=$1" \
+    --data-urlencode "reply_to_message_id=$2" \
+    --data-urlencode "text=$3" >> /dev/null
 }
 
 function send(){
-	curl -s "$BOT_API_URL/sendMessage" \
-	--data-urlencode "chat_id=$1" \
-	--data-urlencode "text=$2" >> /dev/null
+    curl -s "$BOT_API_URL/sendMessage" \
+    --data-urlencode "chat_id=$1" \
+    --data-urlencode "text=$2" >> /dev/null
 }
 
 export -f escape
@@ -25,27 +25,27 @@ export -f reply_send
 export -f send
 
 updates=$(curl -s "$BOT_API_URL/getUpdates" \
-		--data-urlencode "timeout=60")
+        --data-urlencode "timeout=60")
 
 while true; do
-	updates=$(curl -s "$BOT_API_URL/getUpdates" \
-		--data-urlencode "offset=$(( $last_id + 1 ))" \
-		--data-urlencode "timeout=60")
-	updates_count=$(echo "$updates" | jq -r ".result | length")
+    updates=$(curl -s "$BOT_API_URL/getUpdates" \
+        --data-urlencode "offset=$(( $last_id + 1 ))" \
+        --data-urlencode "timeout=60")
+    updates_count=$(echo "$updates" | jq -r ".result | length")
 
 
-	if [ -z $updates_count ] ; then 
-		updates_count=0
-	fi
+    if [ -z $updates_count ] ; then 
+        updates_count=0
+    fi
 
-	last_id=$(echo "$updates" | jq -r ".result[$(( "$updates_count" - 1 ))].update_id")
-	for ((i=0; i<$updates_count; i++)); {
-		export chat_id="$(echo "$updates" | jq ".result[$i].message.chat.id")"
-		export reply_id="$(echo "$updates" | jq ".result[$i].message.reply_to_message.message_id")"
-		export reply_text="$(echo "$updates" | jq ".result[$i].message.reply_to_message.text" | escape )"
-		export message_text="$(echo "$updates" | jq ".result[$i].message.text" | escape )"
-		./commands.sh 
-	}
+    last_id=$(echo "$updates" | jq -r ".result[$(( "$updates_count" - 1 ))].update_id")
+    for ((i=0; i<$updates_count; i++)); {
+        export chat_id="$(echo "$updates" | jq ".result[$i].message.chat.id")"
+        export reply_id="$(echo "$updates" | jq ".result[$i].message.reply_to_message.message_id")"
+        export reply_text="$(echo "$updates" | jq ".result[$i].message.reply_to_message.text" | escape )"
+        export message_text="$(echo "$updates" | jq ".result[$i].message.text" | escape )"
+        ./commands.sh 
+    }
 
-	sleep 0.3
+    sleep 0.3
 done
